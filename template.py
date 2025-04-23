bba0b2313b63feebf93473dd2c3b21a9e3e7b071
Palaciolol <lucaspalacioa@gmail.com>
2025-04-23 15:51:06 -0300

    mudanças main

--- template.py
+++ template.py
@@ -12,7 +12,56 @@ cursor = conn.cursor()
 
 
 sql = """
-COPY *entidade* (atributos, ordem, no, csv)
+COPY *MES* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *ANO* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *MUD_TEMP* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *GERACAO_ENERGIA* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *TIPO_ENERGIA* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *AREA* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *GRUPO* (atributos, ordem, no, csv)
+FROM STDIN WITH (
+    FORMAT CSV,
+    HEADER, *se tiver heaer no csv
+    DELIMITER ',',
+    NULL ''
+)
+COPY *PAIS* (atributos, ordem, no, csv)
 FROM STDIN WITH (
     FORMAT CSV,
     HEADER, *se tiver heaer no csv

b80e531e24134a18e6436ad8492c1b9a1316aacc
Palacio <lucaspalacioa@gmail.com>
2025-04-30 15:25:03 -0300

    Teste dos tipos de energia

--- teste.py
+++ teste.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import psycopg2
 
+df = pd.read_csv('Datasets/energia.csv', usecols=['Category', 'Variable'])
+#variables = df['Variable'].unique()
 
-df = pd.read_csv('Datasets/energia.csv')
-tipos = df['Area type'].unique()
-print(tipos)
+tipos_energia = list(df[df['Category'] == 'Electricity generation']['Variable'].unique())
+tipos_energia.remove('Total Generation')
+print(tipos_energia)

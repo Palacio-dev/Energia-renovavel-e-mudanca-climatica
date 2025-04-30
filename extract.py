9ef9e82e923da8854e6abbb4b2ca513e4411e464
Palacio <lucaspalacioa@gmail.com>
2025-04-30 14:33:45 -0300

    Esqueci que tava na main

---
+++ extract.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from psycopg2._psycopg import connection
+
+def load_anos_meses(cursor: connection):
+    # Os datasets contém dados de anos entre 1960 e 2024
+    for ano in range(1960, 2025):
+        cursor.execute(f'INSERT INTO "ANO" (id) VALUES ({ano}) ON CONFLICT (id) DO NOTHING')
+        for m in range(1, 13):
+            cursor.execute(f'INSERT INTO "MES" (id, numero, id_ano) VALUES (\'{m}/{ano}\', {m}, {ano})')
+
+def load_areas(cursor: connection):
+    df = pd.read_csv('Datasets/energia.csv', usecols=['Area', 'Country code', 'Area type'])
+    areas = df['Area'].unique()
+    for area_nome in areas:
+        linha_area = df[df['Area'] == area_nome]
+        code = linha_area.iloc[0]['Country code']
+        tipo = linha_area.iloc[0]['Area type']
+
+        cursor.execute(f'INSERT INTO "AREA" VALUES ({area_nome}) RETURNING id')
+        area_id = cursor.fetchone()[0]
+
+        if tipo == 'Country':
+            cursor.execute(f'INSERT INTO "PAIS" (id, codigo, nome) VALUES ({area_id},{code},{area_nome})')
+        elif tipo == 'Group':
+            cursor.execute(f'INSERT INTO "GRUPO" (id, codigo, nome) VALUES ({area_id},{code},{area_nome})')
+
+
+
+def load_mudanca_temperatura(cursor: connection):
+    pass
+
+
+def load_geracao_energia(cursor: connection):
+    pass
+
+
+def load_tipo_energia(cursor: connection):
+    pass

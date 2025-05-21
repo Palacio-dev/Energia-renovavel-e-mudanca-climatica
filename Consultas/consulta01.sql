786498e4bae10b59b95bd3f08cc22adc248dad31
TochaFh <rafahoyos08@gmail.com>
2025-05-21 19:00:33 -0300

    consulta 1

--- Consultas/consulta01.sql
+++ Consultas/consulta01.sql
@@ -1,5 +1,10 @@
 -- CONSULTA 01
-WITH renovs AS (
+-- Essa consulta reúne em uma tabela os dados de geração de energia total e renovável (valor bruto e em porcentagem),
+-- emissão de CO2 e aumento de temperatura de cada país por ano
+
+WITH
+-- renovs é uma abstração com as somas de gerações de energia renovável para cada país/ano
+renovs AS (
     SELECT
         GE.id_area,
         GE.id_ano,
@@ -19,6 +24,7 @@ WITH renovs AS (
         GE.id_area,
         GE.id_ano
 ),
+-- renovs é uma abstração com as somas de gerações de energia totais para cada país/ano
 tudo AS (
     SELECT
         GE.id_area,
@@ -35,6 +41,7 @@ tudo AS (
 		GE.unidade_geracao,
 		GE.unidade_emissao
 ),
+-- temp_ano reúne o aumento de temperatura total de cada país em cada ano
 temp_ano AS (
 	SELECT
         MT.id_area,
@@ -46,6 +53,8 @@ temp_ano AS (
         MT.id_ano,
 		MT.id_area
 )
+
+-- tabela final
 SELECT
     a.nome AS area,
     t.id_ano AS ano,
@@ -58,10 +67,12 @@ SELECT
     ta.valor AS aumento_temp
 FROM
     tudo AS t
+    -- join da soma total e de só renováveis pra cada país/ano
     JOIN renovs r ON t.id_area = r.id_area
         AND t.id_ano = r.id_ano
     JOIN temp_ano ta ON t.id_area = ta.id_area
         AND t.id_ano = ta.id_ano
+    -- join para os nomes de cada área
     JOIN "AREA" a ON t.id_area = a.id
 WHERE
     t.total_tudo != 0
